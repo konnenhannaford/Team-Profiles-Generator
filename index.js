0685d4c5f9b4b211437a7c88c2bb80f0aae604b9
konnenhannaford <konnen23@gmail.com>
2021-11-03 00:01:36 +1100

    added code to index.js :

--- index.js
+++ index.js
@@ -1,89 +1,51 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const util = require ('util');
-// const path = require ('path');
+const util = require ('util');
+const path = require ('path');
 // const generateMarkdown = require('./utils/generateMarkdown.js');
-const Employee = require('./lib/Employee')
-const Manager = require('./lib/Manager')
-const Engineer = require('./lib/Engineer')
-const Intern = require('./lib/Intern')
+// const Employee = require('./lib/Employee')
+const generateHTML = require('./src/generateHTML')
+const Manager = require('./lib/manager.js')
+const Engineer = require('./lib/engineer.js')
+const Intern = require('./lib/intern.js')
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        name:"title",
-        message:"Give your Project a title",
+        name:"name",
+        message:"Please provide your name",
         type:"input",
         validate: function (answer) {
             if (answer.length < 1) {
-                return console.log("You must give a valid name for the file");
+                return console.log("You must give a valid name");
             }
             return true;
             }
     },
     {
-        name:"description",
-        message:"Give your Project a description",
+        name:"id",
+        message:"Please provide your Project an id",
         type:"input",
-    },
-    {
-        name:"justification",
-        message:"Give info on purpose of this project and what problems it solves",
-        type:"input",
-    },
-    {
-        name:"technologies",
-        message:"Give info on technologies used in this project",
-        type:"input",
-    },
-    {
-        name:"installation",
-        message:"Give info on installing this project",
-        type:"input",
-    },
-    {
-        name:"usage",
-        message:"Give info on the usage of this project",
-        type:"input",
-    },
-    {
-        name:"contributing",
-        message:"Give details on how to access site and contribute ",
-        type:"input",
-    },
-    {
-        name:"testing",
-        message:"Give details on how this can be tested",
-        type:"input",
-    },
-    {
-        name:"github",
-        message:"Provide a github username",
-        type:"input",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must give a valid id");
+            }
+            return true;
+            }
     },
     {
         name:"email",
-        message:"Provide an email address",
+        message:"Please provide your email",
         type:"input",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must give a valid email");
+            }
+            return true;
+            }
     },
-    {
-        name:"credits",
-        message:"Give info on assets that require attribution and any collaborators",
-        type:"input",
-    },
-    {
-        name: "license",
-        message: "Choose your license:",
-        type: "list",
-        choices: ["No Licence used", "Apache 2.0", "BSD 3", "Eclipse", "GNU General Public license v3", "MIT license", "Mozilla Public license", "Common Development and Distribution license"],
-    },
-    {
-        name: "year",
-        message: "Provide a year of copyright for your license:",
-        type: "input",
-        
-    }];
+    ];
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {
@@ -94,10 +56,9 @@ const writeToFile = (fileName, data) => {
 function init() {
     inquirer.prompt(questions)
     .then(inquirerResponses => {
-      console.log('Generating README...');
-      writeToFile('./output/README.md', generateMarkdown({ ...inquirerResponses }));
+      console.log('Generating card');
+      writeToFile('./src/index.js', generateMarkdown({ ...inquirerResponses }));
 });
   }
-  
 // Function call to initialize app
 init();
